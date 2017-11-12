5161bdf317fd201132f4b9640803e8e8e86c87a9
Sarnam Singh <Sarnam Singh>
2017-11-12 16:02:23 +0530

    initializing project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//import '../assets/css/styles.css';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html' 
+})
+export class AppComponent { 
+  title='Welcome , Your are done with your basic setup';
+}
